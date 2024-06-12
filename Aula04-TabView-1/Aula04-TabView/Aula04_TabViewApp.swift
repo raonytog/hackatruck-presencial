52fe36b57d3c30dc5377bdb3a59dd64a589edb33
Raony Togneri <32366081+raonytog@users.noreply.github.com>
2024-06-11 21:11:57 -0300

    Desafio 1

---
+++ Aula04-TabView-1/Aula04-TabView/Aula04_TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aula04_TabViewApp.swift
+//  Aula04-TabView
+//
+//  Created by Turma02-23 on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Aula04_TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
