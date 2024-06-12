b52af3461961ee1a648d44fb7d37cab926b54b38
Raony Togneri <32366081+raonytog@users.noreply.github.com>
2024-06-11 21:12:26 -0300

    Desafio 2

---
+++ Aula04-TabView-2/Aula04-TabView-2/modView.swift
@@ -0,0 +1,18 @@
+//
+//  modView.swift
+//  Aula04-TabView-2
+//
+//  Created by Turma02-23 on 11/06/24.
+//
+
+import SwiftUI
+
+struct modView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    modView()
+}
